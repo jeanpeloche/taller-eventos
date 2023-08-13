75a03e2c9054aa15b7fddbb68c57a21600d53c2a
jeanpeloche <jeanpelochee@gmail.com>
2023-08-13 18:22:44 -0300

    se detiene el evento

--- eventos.js
+++ eventos.js
@@ -2,6 +2,7 @@ para_evento = document.getElementById("el_div")
 
 function mostrarAlertaDiv(){
     alert("Hola! Soy el div");
+    event.stopPropagation();
 }
 
 para_evento.addEventListener("click", mostrarAlertaDiv);
