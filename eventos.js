12739dd16e92c42dd1a28e827b3abcbd55641fde
jeanpeloche <jeanpelochee@gmail.com>
2024-08-20 20:49:42 -0300

    Evento agregado

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+const boton = document.getElementById("boton-div");
+
+    boton.addEventListener("click", function(){
+        alert("Hola! Soy el div");
+    });
+})
